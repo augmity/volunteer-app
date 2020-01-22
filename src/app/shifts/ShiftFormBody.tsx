0be23b8aba26d6c97274ac0a9ceeec43888442db
augmity <augmity@augmity.com>
2020-01-21 23:47:38 -0800

    fix: date range now in 12h format, default to beginning of the day

--- src/app/shifts/ShiftFormBody.tsx
+++ src/app/shifts/ShiftFormBody.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Form, DatePicker } from 'antd';
 import { WrappedFormUtils } from 'antd/lib/form/Form';
+import moment from 'moment';
 
+const RangePicker = DatePicker.RangePicker;
 
 interface IProps {
   form: WrappedFormUtils;
@@ -10,18 +12,20 @@ interface IProps {
 export const ShiftFormBody: React.FC<IProps> = ({ form, children }) => {
 
   const { getFieldDecorator } = form;
+  const defaultDate = moment().startOf('day');
 
   return (
     <Form layout="vertical">
       <Form.Item label="Date/Time">
         {getFieldDecorator('fromTo', {
+          initialValue: [defaultDate, defaultDate],
           rules: [{
             type: 'array',
             required: true,
             message: 'Please select the date-time range'
           }]
         })(
-          <DatePicker.RangePicker showTime format="YYYY-MM-DD HH:mm:ss" />,
+          <RangePicker format="YYYY-MM-DD HH:mm A" showTime={{ use12Hours: true, format: 'h:mm', minuteStep: 5 }} />,
         )}
       </Form.Item>
       
