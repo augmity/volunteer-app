73d74cdbab5dc97fe7ac7aa09d4c5917ab1aaae7
augmity <augmity@augmity.com>
2020-01-26 23:08:11 -0800

    feat: added "my shifts" functionality

--- src/app/shifts/ShiftsFilters.tsx
+++ src/app/shifts/ShiftsFilters.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { PersonSelector } from '../people';
 import { LocationSelector } from '../locations';
 import { JobSelector } from '../jobs';
 import { Button } from 'antd';
+import { AuthContext } from '../../libs/auth';
 
 
 export interface ShiftFilters {
@@ -29,8 +30,16 @@ const inputStyles = {
 
 export const ShiftsFilters: React.FC<IProps & React.HTMLAttributes<HTMLDivElement>> = ({ filters, onFilterChange, style, className }) => {
 
+  const { currentUser } = useContext(AuthContext);
+
   const onModelChange = (value: Partial<ShiftFilters>) => {
     onFilterChange({...filters, ...value});
+  }
+
+  const myShiftsOnly = () => {
+    if (currentUser) {
+      onModelChange({ person: currentUser.uid });
+    }
   }
   
   return (
@@ -39,6 +48,8 @@ export const ShiftsFilters: React.FC<IProps & React.HTMLAttributes<HTMLDivElemen
       <LocationSelector model={filters.location} onModelChange={(value) => onModelChange({ location: value })} style={inputStyles} />
       <JobSelector model={filters.job} onModelChange={(value) => onModelChange({ job: value })} style={inputStyles} />
       <Button onClick={() => onFilterChange({})}>Clear</Button>
+      <span style={{ flex: 1 }}></span>
+      <Button onClick={myShiftsOnly}>My Shifts</Button>
     </div>
   );
 }
