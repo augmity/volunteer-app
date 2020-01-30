c5d9ff1681f36ceda87e776f97a8b97fe079f076
augmity <augmity@augmity.com>
2020-01-29 22:48:23 -0800

    refactor: displaying more compact grid for mobile

--- src/app/shifts/ShiftsGridView.tsx
+++ src/app/shifts/ShiftsGridView.tsx
@@ -7,6 +7,95 @@ import { PeopleInlineList } from '../people/PeopleInlineList';
 import { ShiftResolved } from './ShiftResolved';
 import { Person } from '../people';
 import { ShiftStatus } from './ShiftStatus';
+import { useMediaQuery } from 'react-responsive';
+
+
+const columnsDesktop = [
+  {
+    title: 'Date',
+    dataIndex: 'fromDateTime',
+    key: 'date',
+    render: (item: Date) => (
+      <span>
+        {moment(item).format('YYYY/MM/DD')}
+      </span>
+    ),
+  },
+  {
+    title: 'Start',
+    dataIndex: 'fromDateTime',
+    key: 'start',
+    render: (item: Date) => (
+      <span>
+        {moment(item).format('h:mm A')}
+      </span>
+    ),
+  },
+  {
+    title: 'End',
+    dataIndex: 'toDateTime',
+    key: 'end',
+    render: (item: Date) => (
+      <span>
+        {moment(item).format('h:mm A')}
+      </span>
+    ),
+  },
+  {
+    title: 'Name',
+    dataIndex: 'job.name',
+    key: 'name',
+  },
+  {
+    title: 'Location',
+    dataIndex: 'location.name',
+    key: 'location',
+  },
+  {
+    title: 'People',
+    dataIndex: 'people',
+    key: 'people',
+    render: (people: Person[]) => (
+      <PeopleInlineList data={people} />
+    ),
+  },
+  {
+    title: 'Status',
+    key: 'status',
+    render: (item: ShiftResolved) => (
+      <ShiftStatus model={item} />
+    ),
+  },
+];
+
+const columnsMobile = [
+  {
+    title: 'Date/Time',
+    key: 'date',
+    render: (item: ShiftResolved) => (
+      <span>
+        {moment(item?.fromDateTime).format('DD/MM/YYYY, h:mm A')} - {moment(item?.toDateTime).format('h:mm A')}
+      </span>
+    ),
+  },
+  {
+    title: 'Shift',
+    key: 'name',
+    render: (item: ShiftResolved) => (
+      <div>
+        <a href="#">{item.job.name}</a>
+        <div>{item.location.name}</div>
+      </div>
+    ),
+  },
+  {
+    title: 'Status',
+    key: 'status',
+    render: (item: ShiftResolved) => (
+      <ShiftStatus model={item} />
+    ),
+  },
+];
 
 
 interface IProps {
@@ -16,6 +105,7 @@ interface IProps {
 export const ShiftsGridView: React.FC<IProps & React.HTMLAttributes<HTMLDivElement>> = ({ data, style }) => {
 
   const [gridData, setGridData] = useState<ShiftResolved[]>();
+  const isBigScreen = useMediaQuery({ minDeviceWidth: 1200 });
 
   useEffect(() => {
     if (data) {
@@ -36,67 +126,10 @@ export const ShiftsGridView: React.FC<IProps & React.HTMLAttributes<HTMLDivEleme
     return <div style={{ padding: 16 }}><Skeleton active /></div>;
   }
 
-  const columns = [
-    {
-      title: 'Date',
-      dataIndex: 'fromDateTime',
-      key: 'date',
-      render: (item: Date) => (
-        <span>
-          {moment(item).format('YYYY/MM/DD')}
-        </span>
-      ),
-    },
-    {
-      title: 'Start',
-      dataIndex: 'fromDateTime',
-      key: 'start',
-      render: (item: Date) => (
-        <span>
-          {moment(item).format('h:mm A')}
-        </span>
-      ),
-    },
-    {
-      title: 'End',
-      dataIndex: 'toDateTime',
-      key: 'end',
-      render: (item: Date) => (
-        <span>
-          {moment(item).format('h:mm A')}
-        </span>
-      ),
-    },
-    {
-      title: 'Name',
-      dataIndex: 'job.name',
-      key: 'name',
-    },
-    {
-      title: 'Location',
-      dataIndex: 'location.name',
-      key: 'location',
-    },
-    {
-      title: 'People',
-      dataIndex: 'people',
-      key: 'people',
-      render: (people: Person[]) => (
-        <PeopleInlineList data={people} />
-      ),
-    },
-    {
-      title: 'Status',
-      key: 'status',
-      render: (item: ShiftResolved) => (
-        <ShiftStatus model={item} />
-      ),
-    },
-  ];
 
   return (
     <div style={style}>
-      <Table columns={columns} dataSource={gridData} rowKey="id" pagination={false} />
+      <Table columns={(!isBigScreen) ? columnsMobile : columnsDesktop} dataSource={gridData} rowKey="id" pagination={false} />
     </div>
   );
 }
